0f77bd62263aac2f826e194b6cff7b9db41dd791
daouloccol88 <Daouda@iMac-de-codeloccol-83.local>
2020-06-18 04:13:54 +0100

    Updates

---
+++ precache-manifest.cfb8627a5af04d29aadcfae9a282d319.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb612c59bc251d1ba9bd85d4ba56afe",
+    "url": "/react-voting-app/index.html"
+  },
+  {
+    "revision": "b11bb2ec9cf70c826739",
+    "url": "/react-voting-app/static/css/main.0875774c.chunk.css"
+  },
+  {
+    "revision": "5e1014e31b702cfd768f",
+    "url": "/react-voting-app/static/js/2.6442d897.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-voting-app/static/js/2.6442d897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b11bb2ec9cf70c826739",
+    "url": "/react-voting-app/static/js/main.223fc27c.chunk.js"
+  },
+  {
+    "revision": "bcf29d16c77a91a66605",
+    "url": "/react-voting-app/static/js/runtime-main.67945dce.js"
+  },
+  {
+    "revision": "218c8ee95706e39aeb6c18d08e72bee7",
+    "url": "/react-voting-app/static/media/smash.218c8ee9.jpg"
+  },
+  {
+    "revision": "23e379a834336e792f575e7f9d5d28fd",
+    "url": "/react-voting-app/static/media/starcraft.23e379a8.jpg"
+  },
+  {
+    "revision": "ec3eb557bac27391baaccb34fe57d057",
+    "url": "/react-voting-app/static/media/witcher.ec3eb557.jpg"
+  },
+  {
+    "revision": "eea4412ac932cdbb9be5e1b0e10212df",
+    "url": "/react-voting-app/static/media/zelda.eea4412a.jpg"
+  }
+]);
